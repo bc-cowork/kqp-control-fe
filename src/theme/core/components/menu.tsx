6669f7e556bc309bf8f38e7927e870ee29109532
Manthika <tharumudu@gmail.com>
2025-03-28 20:30:20 +1300

    Small bug fix

--- src/theme/core/components/menu.tsx
+++ src/theme/core/components/menu.tsx
@@ -8,7 +8,26 @@ const MuiMenuItem: Components<Theme>['MuiMenuItem'] = {
   /** **************************************
    * STYLE
    *************************************** */
-  styleOverrides: { root: ({ theme }) => ({ ...menuItem(theme) }) },
+  styleOverrides: {
+    root: ({ theme }) => ({
+      ...menuItem(theme),
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.text.primary,
+      '&:hover': {
+        backgroundColor: theme.palette.grey[100],
+      },
+      '&:active': {
+        backgroundColor: theme.palette.grey[300],
+      },
+      '&.Mui-selected': {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.common.white,
+        '&:hover': {
+          backgroundColor: theme.palette.primary.dark,
+        },
+      },
+    }),
+  },
 };
 
 // ----------------------------------------------------------------------
