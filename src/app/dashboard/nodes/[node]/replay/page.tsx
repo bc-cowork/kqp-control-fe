a568c49e0cfce5da033bbcb708ca697c5944506b
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2025-11-26 15:21:58 +0500

    chore: added status screen and required stuff

---
+++ src/app/dashboard/nodes/[node]/replay/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Typography, Box } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import React from 'react';
+import { Breadcrumb } from 'src/components/common/Breadcrumb';
+import { DashboardContent } from 'src/layouts/dashboard';
+import { useTranslate } from 'src/locales';
+
+type Props = { params: { node: string } };
+
+export default function Page({ params }: Props) {
+    const { node } = params;
+    const { t } = useTranslate('replay');
+    return (
+        <DashboardContent maxWidth="xl">
+            <Breadcrumb node={node} pages={[{ pageName: t('top.title') }]} />
+            <Typography sx={{ fontSize: 28, fontWeight: 500, color: grey[50], mt: 2 }}>
+                {t('top.title')}
+            </Typography>
+            <Box
+                sx={{
+                    mt: '28px',
+                    width: 1,
+                }}
+            />
+        </DashboardContent >
+    );
+}
