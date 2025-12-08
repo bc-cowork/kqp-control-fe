14d28461172996b5c957a59203e3a36724b6ddff
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2025-12-08 16:17:51 +0500

    chore: daily report list shown

--- src/app/dashboard/nodes/[node]/daily-report-list/page.tsx
+++ src/app/dashboard/nodes/[node]/daily-report-list/page.tsx
@@ -6,24 +6,14 @@ import React from 'react';
 import { Breadcrumb } from 'src/components/common/Breadcrumb';
 import { DashboardContent } from 'src/layouts/dashboard';
 import { useTranslate } from 'src/locales';
+import { ReportListView } from 'src/sections/nodes/report-list/view';
 
 type Props = { params: { node: string } };
 
 export default function Page({ params }: Props) {
     const { node } = params;
-    const { t } = useTranslate('daily-report-list');
+
     return (
-        <DashboardContent maxWidth="xl">
-            <Breadcrumb node={node} pages={[{ pageName: t('top.title') }]} />
-            <Typography sx={{ fontSize: 28, fontWeight: 500, color: grey[50], mt: 2 }}>
-                {t('top.title')}
-            </Typography>
-            <Box
-                sx={{
-                    mt: '28px',
-                    width: 1,
-                }}
-            />
-        </DashboardContent >
+        <ReportListView nodeId={node} />
     );
 }
