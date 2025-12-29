c3c9af88efbfb6184717a6057d6fa96ec8efab96
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2025-12-29 16:02:38 +0500

    chore: more fixes for light theme

--- src/components/nodes/MemoryItem.tsx
+++ src/components/nodes/MemoryItem.tsx
@@ -129,7 +129,9 @@ export function MemoryItem({ selectedNodeId, code }: Props) {
                     <TableErrorRows />
                   ) : (
                     <>
-                      <TableRow sx={{ backgroundColor: '#212447 !important' }}>
+                      <TableRow sx={{
+                        backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#212447 !important' : '#EFF6FF !important'
+                      }}>
                         <TableCell />
                         <TableCell />
                         <TableCell align="right">
@@ -167,8 +169,8 @@ export function MemoryItem({ selectedNodeId, code }: Props) {
                               component="span"
                               sx={{
                                 color: '#7AA2FF',
-                                backgroundColor: '#212447',
-                                border: '1px solid #1D2654',
+                                backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#212447' : '#EFF6FF',
+                                border: (theme) => theme.palette.mode === 'dark' ? '1px solid #1D2654' : '1px solid #DFEAFF',
                                 padding: '2px 4px',
                                 borderRadius: '2px',
                               }}
@@ -195,8 +197,11 @@ export function MemoryItem({ selectedNodeId, code }: Props) {
                           </TableCell>
                         </TableRow>
                       ))}
-                      <TableRow>
-                        <TableCell sx={{ height: '4px', backgroundColor: grey[900] }} colSpan={12} />
+                      <TableRow
+                      >
+                        <TableCell sx={{
+                          backgroundColor: (theme) => theme.palette.mode === 'dark' ? grey[900] : '#F4F4F8'
+                        }} colSpan={12} />
                       </TableRow>
                       {rowKeysBid.map((key: string) => (
                         <TableRow
@@ -208,8 +213,8 @@ export function MemoryItem({ selectedNodeId, code }: Props) {
                               component="span"
                               sx={{
                                 color: '#FF8882',
-                                backgroundColor: '#331B1E',
-                                border: '1px solid #4A2C31',
+                                backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#331B1E' : '#FFF2F4',
+                                border: (theme) => theme.palette.mode === 'dark' ? '1px solid #4A2C31' : '1px solid #FFD8D8',
                                 padding: '2px 4px',
                                 borderRadius: '2px',
                               }}
@@ -236,7 +241,9 @@ export function MemoryItem({ selectedNodeId, code }: Props) {
                           </TableCell>
                         </TableRow>
                       ))}
-                      <TableRow sx={{ backgroundColor: '#331B1E !important' }}>
+                      <TableRow sx={{
+                        backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#331B1E !important' : '#FFF2F4 !important'
+                      }}>
                         <TableCell />
                         <TableCell />
                         <TableCell align="right">
