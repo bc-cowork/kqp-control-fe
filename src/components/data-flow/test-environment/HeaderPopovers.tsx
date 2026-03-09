8765f3537693c0e40989efa9709efb66fd02c354
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2026-03-09 10:28:47 +0500

    chore: updated icons as asked

--- src/components/data-flow/test-environment/HeaderPopovers.tsx
+++ src/components/data-flow/test-environment/HeaderPopovers.tsx
@@ -27,10 +27,13 @@ const DARK_POPOVER_PAPER_SX = {
   bgcolor: '#1A2030',
   border: '1px solid #373F4E',
   boxShadow: '0 8px 24px rgba(0,0,0,0.4)',
-  '& .MuiMenuItem-root': { color: '#E0E4EB' },
+  '& .MuiMenuItem-root': {
+    color: '#E0E4EB',
+    marginBottom: 1,
+  },
   '& .MuiMenuItem-root:hover': { bgcolor: '#2A3344' },
   '& .MuiListItemIcon-root': { color: '#AFB7C8', minWidth: 28 },
-  '& .MuiListItemText-primary': { fontSize: 12 },
+  '& .MuiListItemText-primary': { fontSize: 12, fontWeight: 400 },
 };
 
 // ----------------------------------------------------------------------
@@ -120,10 +123,13 @@ export function HeaderPopovers({
             sx={{ backgroundColor: 'transparent' }}
           >
             <ListItemIcon>
-              <Iconify icon="solar:monitor-outline" width={18} />
+              <svg width="12" height="12" viewBox="0 0 24 24" fill="#E0E4EB" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M3.99995 4.19995C3.00584 4.19995 2.19995 5.00584 2.19995 5.99995V18C2.19995 18.9941 3.00584 19.8 3.99995 19.8H20C20.9941 19.8 21.7999 18.9941 21.7999 18V5.99995C21.7999 5.00584 20.9941 4.19995 20 4.19995H3.99995ZM3.79995 5.99995C3.79995 5.88949 3.88949 5.79995 3.99995 5.79995H8.99998L8.99997 18.2H3.99995C3.88949 18.2 3.79995 18.1104 3.79995 18V5.99995ZM10.6 18.2H20C20.1104 18.2 20.2 18.1104 20.2 18V5.99995C20.2 5.88949 20.1104 5.79995 20 5.79995H10.6L10.6 18.2Z" fill="#E0E4EB" />
+              </svg>
+
             </ListItemIcon>
-            <ListItemText sx={{ flex: 'none' }}>{t('sandbox.view_screen')}</ListItemText>
-            <Iconify icon="eva:chevron-right-fill" width={16} sx={{ ml: 'auto', color: '#AFB7C8' }} />
+            <ListItemText sx={{ flex: 'none', marginLeft: '-22px', marginRight: '18px' }}>{t('sandbox.view_screen')}</ListItemText>
+            <Iconify icon="eva:chevron-right-fill" width={20} sx={{ ml: 'auto', color: '#AFB7C8' }} />
           </MenuItem>
 
           <MenuItem
@@ -134,32 +140,28 @@ export function HeaderPopovers({
             sx={{ backgroundColor: 'transparent' }}
           >
             <ListItemIcon>
-              <Iconify icon="solar:magnifer-outline" width={18} />
+              <svg width="10" height="10" viewBox="0 0 18 18" fill="#E0E4EB" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.2002 0C17.6419 0.000105527 17.9999 0.358132 18 0.799805V8.7998C18 9.24157 17.6419 9.5995 17.2002 9.59961C16.7584 9.59961 16.4004 9.24163 16.4004 8.7998V2.71484L2.54785 16.3984H8.7998C9.24163 16.3984 9.59961 16.7564 9.59961 17.1982C9.5995 17.64 9.24157 17.998 8.7998 17.998H0.799805C0.358132 17.9979 0.000105527 17.6399 0 17.1982V9.19824C0 8.75648 0.358067 8.39854 0.799805 8.39844C1.24163 8.39844 1.59961 8.75641 1.59961 9.19824V15.085L15.252 1.59961H9.2002C8.75837 1.59961 8.40039 1.24163 8.40039 0.799805C8.4005 0.358067 8.75843 0 9.2002 0H17.2002Z" fill="#E0E4EB" />
+              </svg>
             </ListItemIcon>
-            <ListItemText sx={{ flex: 'none' }}>{t('sandbox.interface_scale')}</ListItemText>
-            <Iconify icon="eva:chevron-right-fill" width={16} sx={{ ml: 'auto', color: '#AFB7C8' }} />
+            <ListItemText sx={{ flex: 'none', marginLeft: '-22px', marginRight: '18px' }}>{t('sandbox.interface_scale')}</ListItemText>
+            <Iconify icon="eva:chevron-right-fill" width={20} sx={{ ml: 'auto', color: '#AFB7C8' }} />
           </MenuItem>
 
           <MenuItem onClick={onToggleFullscreen} sx={{ backgroundColor: 'transparent' }}>
             <ListItemIcon>
-              <Iconify
-                icon={
-                  isFullscreen
-                    ? 'solar:quit-full-screen-square-outline'
-                    : 'solar:full-screen-square-outline'
-                }
-                width={18}
-              />
+              <svg width="12" height="12" viewBox="0 0 24 24" fill="#E0E4EB" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.28571 2C5.14907 2 4.05898 2.45153 3.25526 3.25526C2.45153 4.05898 2 5.14907 2 6.28571V17.7143C2 18.8509 2.45153 19.941 3.25526 20.7447C4.05898 21.5485 5.14907 22 6.28571 22H17.7143C18.8509 22 19.941 21.5485 20.7447 20.7447C21.5485 19.941 22 18.8509 22 17.7143V6.28571C22 5.14907 21.5485 4.05898 20.7447 3.25526C19.941 2.45153 18.8509 2 17.7143 2H6.28571ZM3.42857 6.28571C3.42857 5.52795 3.72959 4.80123 4.26541 4.26541C4.80123 3.72959 5.52795 3.42857 6.28571 3.42857H17.7143C18.472 3.42857 19.1988 3.72959 19.7346 4.26541C20.2704 4.80123 20.5714 5.52795 20.5714 6.28571H3.42857ZM3.42857 7.71429H20.5714V17.7143C20.5714 18.472 20.2704 19.1988 19.7346 19.7346C19.1988 20.2704 18.472 20.5714 17.7143 20.5714H6.28571C5.52795 20.5714 4.80123 20.2704 4.26541 19.7346C3.72959 19.1988 3.42857 18.472 3.42857 17.7143V7.71429Z" fill="#E0E4EB" />
+              </svg>
             </ListItemIcon>
-            <ListItemText>
+            <ListItemText sx={{
+              flex: 'none', marginLeft: '-22px', marginRight: '18px',
+            }}>
               {t(isFullscreen ? 'sandbox.exit_fullscreen' : 'sandbox.enter_fullscreen')}
             </ListItemText>
           </MenuItem>
 
           <MenuItem onClick={onExit} sx={{ backgroundColor: 'transparent' }}>
-            <ListItemIcon>
-              <Iconify icon="solar:logout-2-outline" width={18} />
-            </ListItemIcon>
             <ListItemText>{t('sandbox.exit')}</ListItemText>
           </MenuItem>
         </MenuList>
